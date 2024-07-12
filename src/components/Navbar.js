00448a7e2839c7cf3da63ad87bc246c65b805933
Midhun <midhunworks29@gmail.com>
2024-07-12 16:24:06 +0530

    added navigation icon selection color using useLocation hook in react

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,75 +1,47 @@
-// src/components/Navbar.js
-
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import './Navbar.css'; // Import the corresponding CSS file
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const handleDashBoardClick = () => {
-    navigate('/current_happenings');
-  };
+  const handleDashBoardClick = () => navigate('/current_happenings');
+  const handleMeetingsClick = () => navigate('/meetings');
+  const handleBillClick = () => navigate('/payment_history');
+  const handleStatusClick = () => navigate('/camera_status');
+  const handleRoomClick = () => navigate('/room_display');
+  const handleComplaintsClick = () => navigate('/current_complaints');
+  const handleSignOutClick = () => navigate('/');
 
-  const handleMeetingsClick = () => {
-    navigate('/meetings');
-  }
-
-  const handleBillClick = () => {
-    navigate('/payment_history');
-  }
-
-  const handleStatusClick = () => {
-    navigate('/camera_status');
-  }
-
-  const handleRoomClick=() => {
-    navigate('/room_display');
-  }
-  
-  const handleComplaintsClick = () => {
-    navigate('/current_complaints')
-  }
-
-  const handleSignOutClick = () => {
-    navigate('/');
-  }
+  // Function to check if the current path matches the button's path
+  const isActive = (path) => location.pathname === path ? 'active' : '';
 
   return (
     <div className="dashboard-navbar">
       <a data-tooltip="Dashboard">
         <img
           onClick={handleDashBoardClick}
-          className="navbar-img"
+          className={`navbar-img ${isActive('/current_happenings')}`}
           src="../../assets/images/logo.png"
           alt="Logo"
         />
       </a>
       <div className="navbuttons">
-        <button className="navbutton" data-tooltip="Events" onClick={handleMeetingsClick}>
-          <a >
-            <img className="navbar-button-img" src="../../assets/images/tenant/events.png" alt="Events" />
-          </a>
+        <button className={`navbutton ${isActive('/meetings')}`} data-tooltip="Events" onClick={handleMeetingsClick}>
+          <img className="navbar-button-img" src="../../assets/images/tenant/events.png" alt="Events" />
         </button>
-        <button className="navbutton" data-tooltip="Bill History" onClick={handleBillClick}>
-          <a >
-            <img className="navbar-button-img" src="../../assets/images/tenant/bills.png" alt="Bill History" />
-          </a>
+        <button className={`navbutton ${isActive('/payment_history')}`} data-tooltip="Bill History" onClick={handleBillClick}>
+          <img className="navbar-button-img" src="../../assets/images/tenant/bills.png" alt="Bill History" />
         </button>
-        <button className="navbutton" data-tooltip="Status" onClick={handleStatusClick}>
-          <a>
-            <img className="navbar-button-img" src="../../assets/images/tenant/status.png" alt="Status" />
-          </a>
+        <button className={`navbutton ${isActive('/camera_status')}`} data-tooltip="Status" onClick={handleStatusClick}>
+          <img className="navbar-button-img" src="../../assets/images/tenant/status.png" alt="Status" />
         </button>
-        <button className="navbutton" data-tooltip="Complaints" onClick={handleComplaintsClick}>
-          <a >
-            <img className="navbar-button-img" src="../../assets/images/tenant/complaints.png" alt="Complaints" />
-          </a>
+        <button className={`navbutton ${isActive('/current_complaints')}`} data-tooltip="Complaints" onClick={handleComplaintsClick}>
+          <img className="navbar-button-img" src="../../assets/images/tenant/complaints.png" alt="Complaints" />
         </button>
-        <button className="navbutton" data-tooltip="Chats" onClick={handleRoomClick}>
-          <a>
-            <img className="navbar-button-img" src="../../assets/images/tenant/chats.png" alt="Chats" />
-          </a>
+        <button className={`navbutton ${isActive('/room_display')}`} data-tooltip="Chats" onClick={handleRoomClick}>
+          <img className="navbar-button-img" src="../../assets/images/tenant/chats.png" alt="Chats" />
         </button>
       </div>
       <button className="signout" data-tooltip="Sign Out" onClick={handleSignOutClick}>
